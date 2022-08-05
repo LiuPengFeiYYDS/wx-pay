c7095ddafaa5f70ee78e8b3dc79503bd7cad763f
lpf <2792717765@qq.com>
2022-08-05 14:17:24 +0800

    '封装微信小程序'

---
+++ config/config.js
@@ -0,0 +1,5 @@
+// 封装请求的公共路径
+const APIConfig={
+  baseURL:'https://admin.hxwendeng.com'
+}
+export {APIConfig}
---
+++ config/exceptionMessage.js
@@ -0,0 +1,6 @@
+// 请求不同的时候
+const exceptionMessage = {
+  201 : '功能未开发'
+}
+
+export default exceptionMessage
